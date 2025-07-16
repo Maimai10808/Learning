07b1713d4e7736913eb26aead5656cb9d028fbe3
Maimai10808 <331686549@qq.com>
2025-07-16 17:51:42 +0800

    3. math functions

---
+++ C2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+
+
+// 3. math functions
+int main() {
+
+    float x = 3.14;
+
+    // x = pow(x, 4);
+    // x = sqrt(x);
+    // x = round(x);
+    // x = ceil(x);
+    // x = floor(x);
+    // x = abs(x);
+    // x = sin(x);
+    // x = cos(x);
+    // x = tan(x);
+
+    printf("%f", x);
+
+    return 0;
+}
