35f243e5a1f2ee94f6334a35475d589b52c3a732
Maimai10808 <331686549@qq.com>
2025-07-16 20:08:03 +0800

    13. functions

---
+++ C4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+// 13. functions
+
+void happyBirthday() {
+    printf("\nHappy birthday to you!");
+    printf("\nHappy birthday to you!");
+    printf("\nHappy birthday to you!");
+    printf("\nHappy birthday to you!");
+}
+
+void happyBirthdate() {
+    printf("\nHappy birthday to me!");
+    printf("\nHappy birthday to me!");
+    printf("\nHappy birthday to me!");
+    printf("\nHappy birthday to me!");
+}
+
+
+int main() {
+
+    happyBirthday();
+    happyBirthdate();
+
+    return 0;
+}
+
+
+
