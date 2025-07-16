9a1364bdce1e6974d46bc4aa4cceaca496e452a4
Maimai10808 <331686549@qq.com>
2025-07-16 19:41:34 +0800

    7. weight converter

--- C3.c
+++ C3.c
@@ -2,6 +2,7 @@
 #include <string.h>
 
 
+/*
 
 // 6. if statements
 
@@ -22,6 +23,46 @@ int main() {
     }
     else {
         printf("Hello %s", name);
+    }
+
+    return 0;
+}
+
+    */
+
+    // 7. weight converter
+
+int main() {
+
+    // WEIGHT CONVERTER PROGRAM
+
+    int choice = 0;
+    float pounds = 0.0f;
+    float kilograms = 0.0f;
+
+    printf("Weight Conversion Calculator");
+    printf("1. kilograms to Pounds\n");
+    printf("2. Pounds to kilograms\n");
+    printf("Enter your choice (1 or 2): ");
+    scanf("%d", &choice);
+
+    if(choice == 1) {
+        // Kilograms to pounds
+        printf("Enter your weight in kilograms: ");
+        scanf("%f", &kilograms);
+        pounds = kilograms * 2.20462;
+        printf("%.2f kilograms is equal to %.2f pounds\n",kilograms, pounds);
+
+    }
+    else if (choice == 2) {
+        // pounds to Kilograms
+        printf("Enter your weight in pounds: ");
+         scanf("%f", &pounds);
+        kilograms = pounds / 2.20462;
+        printf("%.2f pounds is equal to %.2f kilograms\n",pounds, kilograms);
+    }
+    else {
+        printf("Invalid choice! Please try again\n");
     }
 
     return 0;
