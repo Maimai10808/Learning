18d08d5b6ba8f587f5615df59483d5d0455a9229
Maimai10808 <331686549@qq.com>
2025-07-16 20:12:39 +0800

    13. functions

--- C4.c
+++ C4.c
@@ -16,11 +16,22 @@ void happyBirthdate() {
     printf("\nHappy birthday to me!");
 }
 
+void happy(char name[], int age) {
+    printf("\nHappy birthday to me!");
+    printf("\nHappy birthday to me!");
+    printf("\nHappy birthday dear %s!", name);
+    printf("\nYou are %d years old!\n", age);
+}
+
 
 int main() {
 
+    char name[] = "Bro";
+    int age = 25;
+
     happyBirthday();
     happyBirthdate();
+    happy(name, age);
 
     return 0;
 }
